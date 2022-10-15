1a51ff78565b5f344e38fc076ef14227587315dd
Kaue Pereira <kauefontes@outlook.com>
2022-10-14 23:40:43 -0400

    Added some text and styled them

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto"/>
+            <Text style={styles.title}>React Native React!</Text>
+            <View style={styles.card}>
+                <Text style={styles.cardTitle}>React Native</Text>
+                <Text style={styles.cardSubtitle}>Creating React Native Apps</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#2A2A2A',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        marginBottom: 24
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 12,
+        borderRadius: 3
+    },
+    cardTitle: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    cardSubtitle: {
+        color: '#999',
+        fontSize: 14
+    }
 });
